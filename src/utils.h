3edbe7db20050b993b3a9abb10503d58b85f6a8b
mkomod <mkomodromos01@gmail.com>
2022-02-15 22:54:42 +0000

    up: arma version

--- src/utils.h
+++ src/utils.h
@@ -1,10 +1,10 @@
 #ifndef GSVB_UTILS_H
 #define GSVB_UTILS_H
 
-#include "RcppEigen.h"
+#include "RcppEnsmallen.h"
+
 #include "gsvb_types.h"
 
-std::vector<std::array<int, 2>> get_group_indices(veci);
 double sigmoid(double x);
 
 #endif
